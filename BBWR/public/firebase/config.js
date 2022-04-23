2eb019143eb629efee533a6b7179e8cca2b3b3bc
Peter Hardy <peter@phozart.com>
2022-04-23 12:50:32 +0200

    ot

---
+++ BBWR/public/firebase/config.js
@@ -0,0 +1,12 @@
+
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8ycwxfKXESEtQEte14Tl7V5wnOrqdjqw",
+  authDomain: "reactproject-530a9.firebaseapp.com",
+  projectId: "reactproject-530a9",
+  storageBucket: "reactproject-530a9.appspot.com",
+  messagingSenderId: "570664681954",
+  appId: "1:570664681954:web:3973b9c6894ecd3841f9bb"
+};
+
---
+++ BBWR/server/config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "MyVerySecretKeyThatIsUnKnoWnToAnYOne532165498765431589453dsf15e63q155y9y3n11h55j2566aaatpw4"
+  };
